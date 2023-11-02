b0033e42662edd5244ade986a15ed7308933142d
Sam Peck <fullstacksam@protonmail.com>
2023-11-02 01:09:25 -0400

    Removed App Classcomponet

---
+++ client/src/components/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+// import { authenticate } from './client/src/actions'
+
+function App() {
+
+// useEffect(()=>{
+//   authenticate()
+// },[])
+
+  
+    return (
+      <div className="container">
+        <h1 id="Hello">Amazing how much has changed since 418Y </h1>
+        {/* <Nav />
+        <main>
+          <Routes />
+        </main>
+        <Footer /> */}
+      </div>
+    );
+  
+}
+
+export default App
