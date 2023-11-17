8d518c060a9a7889180791af15a8849cc3df5280
Sam Peck <fullstacksam@protonmail.com>
2023-11-16 23:39:49 -0500

    old asset data

--- backend/db/seeders/20231117042315-assets.js
+++ backend/db/seeders/20231117042315-assets.js
@@ -1,25 +1,80 @@
 'use strict';
-
+//data may have to change basesd on what is avalibe
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('asset', 
+    [
+      {
+        symbol : 'GE',
+        portfolio_id : 1,
+        shares : 100,
+        purchased_at : '2016-10-05'        
+      },
+      {
+        symbol : 'GE',
+        portfolio_id : 2,
+        shares : 12,
+        purchased_at : '2015-10-05'        
+      },
+      {
+        symbol : 'GE',
+        portfolio_id : 3,
+        shares : 13,
+        purchased_at : '2014-10-05'        
+      },
+      {
+        symbol : 'GE',
+        portfolio_id : 4,
+        shares : 21,
+        purchased_at : '2013-10-05'        
+      },
+      {
+        symbol : 'GE',
+        portfolio_id : 5,
+        shares : 23,
+        purchased_at : '2012-10-05'        
+      },
+      {
+        symbol : 'GE',
+        portfolio_id : 6,
+        shares : 43,
+        purchased_at : '2011-10-05'        
+      },
+      {
+        symbol : 'GE',
+        portfolio_id : 7,
+        shares : 60,
+        purchased_at : '2010-10-05'        
+      },
+      {
+        symbol : 'AAPL',
+        portfolio_id : 1,
+        shares : 8,
+        purchased_at : '2017-09-05'        
+      },
+      {
+        symbol : 'GOOG',
+        portfolio_id : 1,
+        shares : 11,
+        purchased_at : '2017-08-05'        
+      },
+      {
+        symbol : 'AMZN',
+        portfolio_id : 1,
+        shares : 14,
+        purchased_at : '2017-07-05'        
+      },
+      {
+        symbol : 'MSFT',
+        portfolio_id : 1,
+        shares : 22,
+        purchased_at : '2017-06-05'        
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('asset', null, {});
   }
 };
