664cf76d4334b11e860299f8f7bca6e7e4237811
Sam Peck <fullstacksam@protonmail.com>
2023-11-16 23:14:57 -0500

    Sequilze init and update

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,12 +1,15 @@
+'use strict';
+
 const 
   fs = require('fs'),
   path = require('path'),
   Sequelize = require('sequelize'),
+  process = require('process'),
   basename = path.basename(__filename),
   env = process.env.NODE_ENV || 'development',
-  config = require(__dirname + '/database.js')[env],
+  config = require(__dirname + '/../config/database.js')[env],
   db = {};
-var sequelize;
+let sequelize;
 
 if(config.use_env_variable){
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -15,22 +18,26 @@ else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
   })
   .forEach(file => {
-    const 
-      model = sequelize['import'](path.join(__dirname, file)),
-      modelKey = model.name.charAt(0).toUpperCase() + model.name.slice(1); // Capitalize Model ref
-    db[modelKey] = model;
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
 
-for(let key in db){
-  if(db[key].associate){
-    db[key].associate(db);
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
-}
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
