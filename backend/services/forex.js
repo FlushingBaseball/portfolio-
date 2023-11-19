75ce35e491ef0d451f7321b059ba18994bc5d840
Sam Peck <fullstacksam@protonmail.com>
2023-11-19 01:51:05 -0500

    Completed Forex Service

---
+++ backend/services/forex.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const qs = require('query-string');
+const moment = require('moment');
+const apiKey = process.env.STOCK_TOKEN;
+const functions = {};
+
+
+functions.getRealTime = async (base, convertTo) => {
+  if (base ===convertTo){return 1};
+
+  const queryString = qs.stringify({
+    base,
+    api_token : apiKey
+  });
+
+  return axios.get(`https://api.worldtradingdata.com/api/v1/forex?${queryString}`)
+    .then((res)=> parseFloat(res.data.data[convertTo]))
+};
+
+functions.getHistorical = async(base, convertTo, dateFrom, dateTo = moment().format('YYYY-MM-DD')) => {
+  if (base ===convertTo){return null};
+
+  const queryString = qs.stringify({
+    base,
+    convert_to : convertTo,
+    date_from : dateFrom,
+    date_to : dateTo,
+    sort : 'newest',
+    api_token : apiKey
+  });
+
+  return axios.get(`https://api.worldtradingdata.com/api/v1/forex_history?${queryString}`)
+    .then((res) => {
+      let history = res.data.history
+      let parsedHistory = {};
+
+      for(let date in history){parsedHistory[date] = parseFloat(history[date]);}
+      return parsedHistory;
+    });
+};
+
+module.exports = functions; 
