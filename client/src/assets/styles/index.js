59da755a88b90a2cb5cea014d55f4b146f832ec9
Sam Peck <fullstacksam@protonmail.com>
2023-11-20 22:41:20 -0500

    starting asset styles

---
+++ client/src/assets/styles/index.js
@@ -0,0 +1,20 @@
+import Colors from '../colors';
+import Fonts from '../fonts';
+
+const title = {
+  fontFamily : Fonts.title,
+  color : 'black' // White when background changes
+};
+
+const dollar = {
+  color : Color.green
+};
+
+const subtitle = {
+  fontFamily : Font.subtitle, 
+  color : "black", //white when background changes
+  fontSize : '2.4rem',
+  marginTop : '3rem'
+}
+
+export {title, dollar, subtitle};
