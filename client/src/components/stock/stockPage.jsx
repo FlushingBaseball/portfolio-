e694c12dda371a1b6343c01a6d2d4445173a0be8
Sam Peck <fullstacksam@protonmail.com>
2023-11-21 19:54:50 -0500

    reducers and starting componets

---
+++ client/src/components/stock/stockPage.jsx
@@ -0,0 +1,15 @@
+import LineGraph from '../graphs/lineGraph'
+import HighLowChart from '../graphs/highLowChart'
+import Header from './stockPageHeader';
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function StockPage(){
+
+
+
+
+
+
+
+}
