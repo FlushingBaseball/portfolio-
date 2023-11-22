66f47435e7da91c35dadec9011ff03084978c75c
Sam Peck <fullstacksam@protonmail.com>
2023-11-21 23:22:10 -0500

    Dependencies

---
+++ client/src/components/crud/buttons/newPortfolio.jsx
@@ -0,0 +1,9 @@
+import colors from '../../../assets/colors'
+import Popup from 'reactjs-popup'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faPlus } from `@fortawesome/free-solid-svg-icons`
+import TextField from '@material-ui/core/TextField';
+import Autocompletet from `@material-ul/lab/Autocomplete`;
+import axios from 'axios';
+import {stringify as queryStringify } from 'query-string'
+
