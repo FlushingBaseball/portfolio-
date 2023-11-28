fc5eb1ffc8fdd1e505784d8ea5ddb2de749f62a6
Sam Peck <fullstacksam@protonmail.com>
2023-11-27 23:18:19 -0500

    new asset

---
+++ client/src/components/crud/buttons/newAsset.jsx
@@ -0,0 +1,8 @@
+import colors from '../../../assets/colors'
+import Popup from 'reactjs-popup'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+// import TextField from '@material'
+
+<input id ='TextField' aria-invalid='false'
+ />
