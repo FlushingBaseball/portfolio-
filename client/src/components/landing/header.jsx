dd8c360b03fab2c117238eff51c075c41b171707
Sam Peck <fullstacksam@protonmail.com>
2023-12-08 01:07:51 -0500

    header

---
+++ client/src/components/landing/header.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { title, dollar, subtitle } from "../../assets/styles";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1 style={title}>
+          Portfolio<span style={dollar}>$</span>
+        </h1>
+        <p style={subtitle}>
+          A <em>cutting edge</em>, minimalist's portfolio tracking web app.
+        </p>
+        <p>What would you do if you were given $100,000?</p>
+        <p>
+          Would you spend it all at once? Would you think about putting some of
+          it in the bank?
+        </p>
+        <p>We’d like to teach you how to invest it, so you might grow it.</p>
+        <p>
+          Through Portfolios, you will gain a fundamental understanding of
+          investing and how you might get your money to work for you.
+        </p>
+        <Link to="/packages">
+          Sign Up <i className="fas fa-chevron-circle-right"></i>
+        </Link>
+      </div>
+    </header>
+  );
+}
